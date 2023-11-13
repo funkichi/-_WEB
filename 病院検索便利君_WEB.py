89c30a11f35f7af442f9f5e56150e6263220f5f0
funkichi <funkynaokichi@gmail.com>
2023-11-14 07:16:43 +0900

    地図以外の検索動作は正常

--- 病院検索便利君_WEB.py
+++ 病院検索便利君_WEB.py
@@ -24,32 +24,29 @@ with st.form(key='my_form'):
         if selected_option == "オンライン診療":
             if selected_location in df["市町村"].values:
                 column_name_1 = ["医療機関名", "診療時間_午前", "診療時間_午後"]
-                selected_row = df[(df["市町村"] == selected_location) & (df[selected_week] == 1) & (df["オンライン診療"] == 1)]
+                selected_row = df[(df["市町村"] == selected_location) & (df[selected_week] == "〇") & (df["オンライン診療"] == "〇")]
                 st.dataframe(selected_row[column_name_1])
-                selected_row_index = st.selectbox("選択", selected_row.index)
+                selected_row_index = st.selectbox("番号を選択して検索ボタンを押してください", selected_row.index)
                 
-                # コールバックをst.form_submit_buttonに移動
-                st.form_submit_button("詳細", on_click=lambda: st.table(selected_row.loc[selected_row_index, column_name_2]))
-        
+                st.table(selected_row.loc[selected_row_index, column_name_2])
+
         elif selected_option == "夜間診療":
             if selected_location in df["市町村"].values:
                 column_name_1 = ["医療機関名", "診療時間_午前", "診療時間_午後"]
-                selected_row = df[(df["市町村"] == selected_location) & (df[selected_week] == 1) & (df["夜間診療"] == 1)]
+                selected_row = df[(df["市町村"] == selected_location) & (df[selected_week] == "〇") & (df["夜間診療"] == "〇")]
                 st.dataframe(selected_row[column_name_1])
-                selected_row_index = st.selectbox("選択", selected_row.index)
+                selected_row_index = st.selectbox("番号を選択して検索ボタンを押してください", selected_row.index)
                 
-                # コールバックをst.form_submit_buttonに移動
-                st.form_submit_button("詳細", on_click=lambda: st.table(selected_row.loc[selected_row_index, column_name_2]))
-        
+                st.table(selected_row.loc[selected_row_index, column_name_2])
+
         elif selected_option == "選択なし":
             if selected_location in df["市町村"].values:
                 column_name_1 = ["医療機関名", "診療時間_午前", "診療時間_午後"]
-                selected_row = df[(df["市町村"] == selected_location) & (df[selected_week] == 1)]
+                selected_row = df[(df["市町村"] == selected_location) & (df[selected_week] == "〇")]
                 st.dataframe(selected_row[column_name_1])
-                selected_row_index = st.selectbox("選択", selected_row.index)
+                selected_row_index = st.selectbox("番号を選択して検索ボタンを押してください", selected_row.index)
                 
-                # コールバックをst.form_submit_buttonに移動
-                st.form_submit_button("詳細", on_click=lambda: st.table(selected_row.loc[selected_row_index, column_name_2]))
+                st.table(selected_row.loc[selected_row_index, column_name_2])
 
 map = df["MAP"]
-st.sidebar.write(f"[画像保管場所]({map[0]})")
+st.sidebar.write(f"[地図]({map[0]})")
