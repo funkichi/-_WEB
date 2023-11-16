83db3143573f754c1d7939ac5da1c8ba055222de
funkichi <funkynaokichi@gmail.com>
2023-11-16 20:24:38 +0900

    文字をカラーに変更、サイドバー廃止

--- 病院検索便利君_WEB.py
+++ 病院検索便利君_WEB.py
@@ -1,9 +1,10 @@
+
 import streamlit as st
 import pandas as pd
 from PIL import Image
 
 #タイトルの表示をセンタリングするためにhtmlで記述
-st.markdown("<h1 style='text-align: center;'>病院検索便利君</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: lightpink; '>🏥病院検索便利君🏥</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>☆徳島の小児科を検索できるアプリです☆</p>", unsafe_allow_html=True)
 
 #pillow(PIL)で画像を挿入してオプションでcaptionを追加
@@ -14,14 +15,15 @@ st.image(image, caption='早めの受診をおすすめします')
 for i in range(3):
     st.write('\n')
 
-#pandasでcsvファイルを読み込む
+#pandasでcsvファイルを読み込んでセレクトボックスを配置する
 df = pd.read_csv('DataBase/徳島県.csv')
-#"市町村"カラムから重複箇所を削除してlocationに代入
 Location = list(df['市町村'].drop_duplicates())
-#各変数にセレクトボックスで選択された値を代入
-selected_location = st.selectbox('市町村を選択してください', Location)
-selected_week = st.selectbox('受診希望日を選択してください', ['月', '火', '水', '木', '金', '土', '日', '祝日'])
-selected_option = st.selectbox('オプションを選択してください', ["選択なし", "オンライン診療", "夜間診療"])
+st.markdown("<h5 style='text-align: left; color: salmon;'>市町村を選択してください</h5>", unsafe_allow_html=True)
+selected_location = st.selectbox("", Location, label_visibility="collapsed")
+st.markdown("<h5 style='text-align: left; color: darkturquoise;'>受診希望日を選択してください</h5>", unsafe_allow_html=True)
+selected_week = st.selectbox('', ['月', '火', '水', '木', '金', '土', '日', '祝日'], label_visibility="collapsed")
+st.markdown("<h5 style='text-align: left; color: deepskyblue;'>オプションを選択してください</h5>", unsafe_allow_html=True)
+selected_option = st.selectbox('', ["選択なし", "オンライン診療", "夜間診療"], label_visibility="collapsed")
 
 #with内で使用する変数に事前に代入しておく(検索結果の詳細に表示したいカラム名を指定)
 column_name_2 = ["医療機関名", "住所", "電話番号", "医師名", "診療時間_午前", "診療時間_午後", "月", "火", "水", "木", "金", "土", "日", "祝日", "休診日_1", "休診日_2", "休診日_3", "オンライン診療", "夜間診療", "特徴"]
@@ -41,9 +43,11 @@ with st.form(key='my_form'):
                 #照合により一致した行データの中から、表示したいカラム名をcolumn_name1で指定して一覧を表示する
                 st.dataframe(selected_row[column_name_1])
                 #表示された一覧データの行番号をセレクトボックスに反映
-                selected_row_index = st.selectbox("行番号を選択して検索ボタンを押してください", selected_row.index)                
+                st.markdown("<h5 style='text-align: left; color: salmon;'>行番号を選択して検索ボタンを押してください</h5>", unsafe_allow_html=True)
+                selected_row_index = st.selectbox("", selected_row.index, label_visibility="collapsed")            
                 #セレクトボックスで行番号を選択し、再度検索ボタンを押すと選択した行番号にあるデータの詳細が表示される
                 #elected_row_indexが表示したい行、column_name_2が表示したい列(カラム名)
+                st.write(selected_row.loc[selected_row_index, "医療機関名"] + "の詳細情報")
                 st.table(selected_row.loc[selected_row_index, column_name_2])
         
                 #mapにgoogleマップのアドレスを代入し、map_nameにリンク用の表示文字を代入する(↑詳細の表示とリンクするようにselected_row_indexで行指定する)
@@ -51,31 +55,35 @@ with st.form(key='my_form'):
                 map_name = selected_row.loc[selected_row_index, ["医療機関名"]]
 
                 #アドレスが長いのでmap_name変数をリンクの表示文字に指定してmap変数でアドレスを指定する(文字列内で変数を使用する為にf-stringsを使う)
-                st.sidebar.write("↓googleマップ↓")
-                st.sidebar.write(f"[{map_name[0]}]({map[0]})")
+                st.markdown("<h5 style='text-align: left; color: darkturquoise;'>Googleマップ</h5>", unsafe_allow_html=True)
+                st.write(f"[{map_name[0]}]({map[0]})")
                 
         elif selected_option == "夜間診療":
             if selected_location in df["市町村"].values:
                 column_name_1 = ["医療機関名", "診療時間_午前", "診療時間_午後"]
                 selected_row = df[(df["市町村"] == selected_location) & (df[selected_week] == "〇") & (df["夜間診療"] == "〇")]
                 st.dataframe(selected_row[column_name_1])
-                selected_row_index = st.selectbox("行番号を選択して検索ボタンを押してください", selected_row.index)                
+                st.markdown("<h5 style='text-align: left; color: salmon;'>行番号を選択して検索ボタンを押してください</h5>", unsafe_allow_html=True)
+                selected_row_index = st.selectbox("", selected_row.index, label_visibility="collapsed") 
+                st.write(selected_row.loc[selected_row_index, "医療機関名"] + "の詳細情報")                
                 st.table(selected_row.loc[selected_row_index, column_name_2])
                 
                 map = selected_row.loc[selected_row_index, ["MAP"]]
                 map_name = selected_row.loc[selected_row_index, ["医療機関名"]]
-                st.sidebar.write("↓googleマップ↓")
-                st.sidebar.write(f"[{map_name[0]}]({map[0]})")
+                st.markdown("<h5 style='text-align: left; color: darkturquoise;'>Googleマップ</h5>", unsafe_allow_html=True)
+                st.write(f"[{map_name[0]}]({map[0]})")
 
         elif selected_option == "選択なし":
             if selected_location in df["市町村"].values:
                 column_name_1 = ["医療機関名", "診療時間_午前", "診療時間_午後"]
                 selected_row = df[(df["市町村"] == selected_location) & (df[selected_week] == "〇")]
                 st.dataframe(selected_row[column_name_1])
-                selected_row_index = st.selectbox("行番号を選択して検索ボタンを押してください", selected_row.index)                
+                st.markdown("<h5 style='text-align: left; color: salmon;'>行番号を選択して検索ボタンを押してください</h5>", unsafe_allow_html=True)
+                selected_row_index = st.selectbox("", selected_row.index, label_visibility="collapsed") 
+                st.write(selected_row.loc[selected_row_index, "医療機関名"] + "の詳細情報")                
                 st.table(selected_row.loc[selected_row_index, column_name_2])
                 
                 map = selected_row.loc[selected_row_index, ["MAP"]]
                 map_name = selected_row.loc[selected_row_index, ["医療機関名"]]
-                st.sidebar.write("↓googleマップ↓")
-                st.sidebar.write(f"[{map_name[0]}]({map[0]})")
+                st.markdown("<h5 style='text-align: left; color: darkturquoise;'>Googleマップ</h5>", unsafe_allow_html=True)
+                st.write(f"[{map_name[0]}]({map[0]})")
